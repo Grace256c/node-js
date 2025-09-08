1785e84b3f1015ad8a36f71fae191d98004ded7b
unknown <MUTEZIMANA@SI-OPT018.ict.innovationhub.ug>
2025-09-08 11:59:38 +0300

    nodejs

---
+++ index.js
@@ -0,0 +1,13 @@
+const express= require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/',(reqest,response)=>{
+    response.send('Hello World');
+})
+
+app.listen(PORT,()=>{
+    console.log("Server is running on port" + PORT);
+})
